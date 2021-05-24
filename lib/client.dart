b065d935b22d91d519462dea64abba1c39437977
Eldad Fux <eldad.fux@gmail.com>
2021-05-24 12:40:37 +0300

    Fixed deployment bug

--- lib/client.dart
+++ lib/client.dart
@@ -26,7 +26,7 @@ class Client {
         
         this.headers = {
             'content-type': 'application/json',
-            'x-sdk-version': 'appwrite:flutter:0.6.1',
+            'x-sdk-version': 'appwrite:flutter:0.6.2',
             'X-Appwrite-Response-Format' : '0.8.0',
         };
 
