933c695a8e66772223904c7dc92746c354aa15f2
Eldad Fux <eldad.fux@gmail.com>
2021-05-24 09:30:09 +0300

    Update version number

--- lib/client.dart
+++ lib/client.dart
@@ -26,7 +26,7 @@ class Client {
         
         this.headers = {
             'content-type': 'application/json',
-            'x-sdk-version': 'appwrite:flutter:0.6.0',
+            'x-sdk-version': 'appwrite:flutter:0.6.1',
             'X-Appwrite-Response-Format' : '0.8.0',
         };
 
