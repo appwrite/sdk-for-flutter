c6a0d8f923699b4a88f30ff8d2b822cc5b34a82d
Damodar Lohani <dlohani48@gmail.com>
2022-03-03 17:44:59 +0000

    fix the param

--- lib/src/input_file.dart
+++ lib/src/input_file.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' show MultipartFile;
 class InputFile {
   final MultipartFile? file;
   final String? path;
-  final String? fileName;
+  final String? filename;
 
-  InputFile({this.file, this.path, this.fileName});
+  InputFile({this.file, this.path, this.filename});
 }
