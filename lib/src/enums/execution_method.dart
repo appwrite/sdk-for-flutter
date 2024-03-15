c4fed2107d819c387b734d32c08a16db2ae131d9
GoltVik <credowolf777@gmail.com>
2024-03-15 21:57:23 +0100

    fixed ExecutionMethod enum naming

--- lib/src/enums/execution_method.dart
+++ lib/src/enums/execution_method.dart
@@ -1,18 +1,16 @@
 part of appwrite.enums;
 
 enum ExecutionMethod {
-    gET(value: 'GET'),
-    pOST(value: 'POST'),
-    pUT(value: 'PUT'),
-    pATCH(value: 'PATCH'),
-    dELETE(value: 'DELETE'),
-    oPTIONS(value: 'OPTIONS');
+  get(value: 'GET'),
+  post(value: 'POST'),
+  put(value: 'PUT'),
+  patch(value: 'PATCH'),
+  delete(value: 'DELETE'),
+  options(value: 'OPTIONS');
 
-    const ExecutionMethod({
-        required this.value
-    });
+  const ExecutionMethod({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
