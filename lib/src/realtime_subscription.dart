8b01d6f309d0579a7945ff093d8ab0888c272306
Damodar Lohani <dlohani48@gmail.com>
2024-04-25 01:49:20 +0000

    fix changelog

--- lib/src/realtime_subscription.dart
+++ lib/src/realtime_subscription.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'realtime_message.dart';
 
 /// Realtime Subscription
@@ -5,9 +7,16 @@ class RealtimeSubscription {
   /// Stream of [RealtimeMessage]s
   final Stream<RealtimeMessage> stream;
 
+  final StreamController<RealtimeMessage> controller;
+
+  /// List of channels
+  List<String> channels;
+
   /// Closes the subscription
   final Future<void> Function() close;
 
   /// Initializes a [RealtimeSubscription]
-  RealtimeSubscription({required this.stream, required this.close});
+  RealtimeSubscription(
+      {required this.close, required this.channels, required this.controller})
+      : stream = controller.stream;
 }
