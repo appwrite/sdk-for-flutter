f00863d5f757a75d63aa7fe53581ef973be28e6c
christyjacob4 <christyjacob4@users.noreply.github.com>
2024-08-20 11:03:08 +0000

    Commit from GitHub Actions (Format and push)

--- lib/query.dart
+++ lib/query.dart
@@ -1,6 +1,5 @@
 part of 'appwrite.dart';
 
-
 /// Helper class to generate query strings.
 class Query {
   final String method;
@@ -14,11 +13,11 @@ class Query {
       'method': method,
     };
 
-    if(attribute != null) {
+    if (attribute != null) {
       map['attribute'] = attribute;
     }
-    
-    if(values != null) {
+
+    if (values != null) {
       map['values'] = values is List ? values : [values];
     }
 
@@ -29,7 +28,7 @@ class Query {
   String toString() => jsonEncode(toJson());
 
   /// Filter resources where [attribute] is equal to [value].
-  /// 
+  ///
   /// [value] can be a single value or a list. If a list is used
   /// the query will return resources where [attribute] is equal
   /// to any of the values in the list.
@@ -61,10 +60,12 @@ class Query {
       Query._('search', attribute, value).toString();
 
   /// Filter resources where [attribute] is null.
-  static String isNull(String attribute) => Query._('isNull', attribute).toString();
+  static String isNull(String attribute) =>
+      Query._('isNull', attribute).toString();
 
   /// Filter resources where [attribute] is not null.
-  static String isNotNull(String attribute) => Query._('isNotNull', attribute).toString();
+  static String isNotNull(String attribute) =>
+      Query._('isNotNull', attribute).toString();
 
   /// Filter resources where [attribute] is between [start] and [end] (inclusive).
   static String between(String attribute, dynamic start, dynamic end) =>
@@ -84,40 +85,46 @@ class Query {
       Query._('contains', attribute, value).toString();
 
   static String or(List<String> queries) =>
-    Query._('or', null, queries.map((query) => jsonDecode(query)).toList()).toString();
+      Query._('or', null, queries.map((query) => jsonDecode(query)).toList())
+          .toString();
 
   static String and(List<String> queries) =>
-      Query._('and', null, queries.map((query) => jsonDecode(query)).toList()).toString();
+      Query._('and', null, queries.map((query) => jsonDecode(query)).toList())
+          .toString();
 
   /// Specify which attributes should be returned by the API call.
   static String select(List<String> attributes) =>
       Query._('select', null, attributes).toString();
 
   /// Sort results by [attribute] ascending.
-  static String orderAsc(String attribute) => Query._('orderAsc', attribute).toString();
+  static String orderAsc(String attribute) =>
+      Query._('orderAsc', attribute).toString();
 
   /// Sort results by [attribute] descending.
-  static String orderDesc(String attribute) => Query._('orderDesc', attribute).toString();
+  static String orderDesc(String attribute) =>
+      Query._('orderDesc', attribute).toString();
 
   /// Return results before [id].
-  /// 
+  ///
   /// Refer to the [Cursor Based Pagination](https://appwrite.io/docs/pagination#cursor-pagination)
   /// docs for more information.
-  static String cursorBefore(String id) => Query._('cursorBefore', null, id).toString();
+  static String cursorBefore(String id) =>
+      Query._('cursorBefore', null, id).toString();
 
   /// Return results after [id].
-  /// 
+  ///
   /// Refer to the [Cursor Based Pagination](https://appwrite.io/docs/pagination#cursor-pagination)
   /// docs for more information.
-  static String cursorAfter(String id) => Query._('cursorAfter', null, id).toString();
+  static String cursorAfter(String id) =>
+      Query._('cursorAfter', null, id).toString();
 
   /// Return only [limit] results.
   static String limit(int limit) => Query._('limit', null, limit).toString();
 
   /// Return results from [offset].
-  /// 
+  ///
   /// Refer to the [Offset Pagination](https://appwrite.io/docs/pagination#offset-pagination)
   /// docs for more information.
-  static String offset(int offset) => Query._('offset', null, offset).toString();
-
+  static String offset(int offset) =>
+      Query._('offset', null, offset).toString();
 }
