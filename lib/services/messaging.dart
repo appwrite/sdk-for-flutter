39032cb3ea3e59eb73b335bff5b02ef0cff04d4f
root <christyjacob4@gmail.com>
2024-08-23 15:25:13 +0000

    chore: update SDKs for appwrite 1.6.0

--- lib/services/messaging.dart
+++ lib/services/messaging.dart
@@ -3,53 +3,46 @@ part of '../appwrite.dart';
 /// The Messaging service allows you to send messages to any provider type
 /// (SMTP, push notification, SMS, etc.).
 class Messaging extends Service {
-  /// Initializes a [Messaging] service
-  Messaging(super.client);
+    /// Initializes a [Messaging] service
+    Messaging(super.client);
 
-  /// Create subscriber
-  ///
-  /// Create a new subscriber.
-  Future<models.Subscriber> createSubscriber(
-      {required String topicId,
-      required String subscriberId,
-      required String targetId}) async {
-    final String apiPath = '/messaging/topics/{topicId}/subscribers'
-        .replaceAll('{topicId}', topicId);
+    /// Create subscriber
+    ///
+    /// Create a new subscriber.
+    Future<models.Subscriber> createSubscriber({required String topicId, required String subscriberId, required String targetId}) async {
+        final String apiPath = '/messaging/topics/{topicId}/subscribers'.replaceAll('{topicId}', topicId);
 
-    final Map<String, dynamic> apiParams = {
-      'subscriberId': subscriberId,
-      'targetId': targetId,
-    };
+        final Map<String, dynamic> apiParams = {
+            'subscriberId': subscriberId,
+            'targetId': targetId,
+        };
 
-    final Map<String, String> apiHeaders = {
-      'content-type': 'application/json',
-    };
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    final res = await client.call(HttpMethod.post,
-        path: apiPath, params: apiParams, headers: apiHeaders);
+        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
 
-    return models.Subscriber.fromMap(res.data);
-  }
+        return models.Subscriber.fromMap(res.data);
 
-  /// Delete subscriber
-  ///
-  /// Delete a subscriber by its unique ID.
-  Future deleteSubscriber(
-      {required String topicId, required String subscriberId}) async {
-    final String apiPath =
-        '/messaging/topics/{topicId}/subscribers/{subscriberId}'
-            .replaceAll('{topicId}', topicId)
-            .replaceAll('{subscriberId}', subscriberId);
+    }
 
-    final Map<String, dynamic> apiParams = {};
+    /// Delete subscriber
+    ///
+    /// Delete a subscriber by its unique ID.
+    Future deleteSubscriber({required String topicId, required String subscriberId}) async {
+        final String apiPath = '/messaging/topics/{topicId}/subscribers/{subscriberId}'.replaceAll('{topicId}', topicId).replaceAll('{subscriberId}', subscriberId);
 
-    final Map<String, String> apiHeaders = {
-      'content-type': 'application/json',
-    };
+        final Map<String, dynamic> apiParams = {
+        };
 
-    final res = await client.call(HttpMethod.delete,
-        path: apiPath, params: apiParams, headers: apiHeaders);
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    return res.data;
-  }
+        final res = await client.call(HttpMethod.delete, path: apiPath, params: apiParams, headers: apiHeaders);
+
+        return  res.data;
+
+    }
 }
--- lib/src/models/template_variable.dart
+++ lib/src/models/template_variable.dart
@@ -2,52 +2,47 @@ part of '../../models.dart';
 
 /// Template Variable
 class TemplateVariable implements Model {
-  /// Variable Name.
-  final String name;
+    /// Variable Name.
+    final String name;
+    /// Variable Description.
+    final String description;
+    /// Variable Value.
+    final String value;
+    /// Variable Placeholder.
+    final String placeholder;
+    /// Is the variable required?
+    final bool xrequired;
+    /// Variable Type.
+    final String type;
 
-  /// Variable Description.
-  final String description;
+    TemplateVariable({
+        required this.name,
+        required this.description,
+        required this.value,
+        required this.placeholder,
+        required this.xrequired,
+        required this.type,
+    });
 
-  /// Variable Value.
-  final String value;
+    factory TemplateVariable.fromMap(Map<String, dynamic> map) {
+        return TemplateVariable(
+            name: map['name'].toString(),
+            description: map['description'].toString(),
+            value: map['value'].toString(),
+            placeholder: map['placeholder'].toString(),
+            xrequired: map['required'],
+            type: map['type'].toString(),
+        );
+    }
 
-  /// Variable Placeholder.
-  final String placeholder;
-
-  /// Is the variable required?
-  final bool xrequired;
-
-  /// Variable Type.
-  final String type;
-
-  TemplateVariable({
-    required this.name,
-    required this.description,
-    required this.value,
-    required this.placeholder,
-    required this.xrequired,
-    required this.type,
-  });
-
-  factory TemplateVariable.fromMap(Map<String, dynamic> map) {
-    return TemplateVariable(
-      name: map['name'].toString(),
-      description: map['description'].toString(),
-      value: map['value'].toString(),
-      placeholder: map['placeholder'].toString(),
-      xrequired: map['required'],
-      type: map['type'].toString(),
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      "name": name,
-      "description": description,
-      "value": value,
-      "placeholder": placeholder,
-      "required": xrequired,
-      "type": type,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "name": name,
+            "description": description,
+            "value": value,
+            "placeholder": placeholder,
+            "required": xrequired,
+            "type": type,
+        };
+    }
 }
