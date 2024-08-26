cac7045c8fc05f5c32aa482cf968bd16b7fd1545
christyjacob4 <christyjacob4@users.noreply.github.com>
2024-08-26 16:32:43 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/document.dart
+++ lib/src/models/document.dart
@@ -2,53 +2,58 @@ part of '../../models.dart';
 
 /// Document
 class Document implements Model {
-    /// Document ID.
-    final String $id;
-    /// Collection ID.
-    final String $collectionId;
-    /// Database ID.
-    final String $databaseId;
-    /// Document creation date in ISO 8601 format.
-    final String $createdAt;
-    /// Document update date in ISO 8601 format.
-    final String $updatedAt;
-    /// Document permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
-    final List $permissions;
-    final Map<String, dynamic> data;
+  /// Document ID.
+  final String $id;
 
-    Document({
-        required this.$id,
-        required this.$collectionId,
-        required this.$databaseId,
-        required this.$createdAt,
-        required this.$updatedAt,
-        required this.$permissions,
-        required this.data,
-    });
+  /// Collection ID.
+  final String $collectionId;
 
-    factory Document.fromMap(Map<String, dynamic> map) {
-        return Document(
-            $id: map['\$id'].toString(),
-            $collectionId: map['\$collectionId'].toString(),
-            $databaseId: map['\$databaseId'].toString(),
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            $permissions: map['\$permissions'] ?? [],
-            data: map,
-        );
-    }
+  /// Database ID.
+  final String $databaseId;
 
-    Map<String, dynamic> toMap() {
-        return {
-            "\$id": $id,
-            "\$collectionId": $collectionId,
-            "\$databaseId": $databaseId,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "\$permissions": $permissions,
-            "data": data,
-        };
-    }
+  /// Document creation date in ISO 8601 format.
+  final String $createdAt;
 
-    T convertTo<T>(T Function(Map) fromJson) => fromJson(data);
+  /// Document update date in ISO 8601 format.
+  final String $updatedAt;
+
+  /// Document permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
+  final List $permissions;
+  final Map<String, dynamic> data;
+
+  Document({
+    required this.$id,
+    required this.$collectionId,
+    required this.$databaseId,
+    required this.$createdAt,
+    required this.$updatedAt,
+    required this.$permissions,
+    required this.data,
+  });
+
+  factory Document.fromMap(Map<String, dynamic> map) {
+    return Document(
+      $id: map['\$id'].toString(),
+      $collectionId: map['\$collectionId'].toString(),
+      $databaseId: map['\$databaseId'].toString(),
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      $permissions: map['\$permissions'] ?? [],
+      data: map,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "\$id": $id,
+      "\$collectionId": $collectionId,
+      "\$databaseId": $databaseId,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "\$permissions": $permissions,
+      "data": data,
+    };
+  }
+
+  T convertTo<T>(T Function(Map) fromJson) => fromJson(data);
 }
--- lib/src/models/mfa_type.dart
+++ lib/src/models/mfa_type.dart
@@ -2,27 +2,28 @@ part of '../../models.dart';
 
 /// MFAType
 class MfaType implements Model {
-    /// Secret token used for TOTP factor.
-    final String secret;
-    /// URI for authenticator apps.
-    final String uri;
+  /// Secret token used for TOTP factor.
+  final String secret;
 
-    MfaType({
-        required this.secret,
-        required this.uri,
-    });
+  /// URI for authenticator apps.
+  final String uri;
 
-    factory MfaType.fromMap(Map<String, dynamic> map) {
-        return MfaType(
-            secret: map['secret'].toString(),
-            uri: map['uri'].toString(),
-        );
-    }
+  MfaType({
+    required this.secret,
+    required this.uri,
+  });
 
-    Map<String, dynamic> toMap() {
-        return {
-            "secret": secret,
-            "uri": uri,
-        };
-    }
+  factory MfaType.fromMap(Map<String, dynamic> map) {
+    return MfaType(
+      secret: map['secret'].toString(),
+      uri: map['uri'].toString(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "secret": secret,
+      "uri": uri,
+    };
+  }
 }
