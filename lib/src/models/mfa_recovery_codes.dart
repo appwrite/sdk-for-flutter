e2227696801f5908e053381efd90af3192463457
root <christyjacob4@gmail.com>
2024-08-26 16:32:13 +0000

    chore: update for appwrite 1.6.x

--- lib/src/models/mfa_recovery_codes.dart
+++ lib/src/models/mfa_recovery_codes.dart
@@ -2,22 +2,22 @@ part of '../../models.dart';
 
 /// MFA Recovery Codes
 class MfaRecoveryCodes implements Model {
-  /// Recovery codes.
-  final List recoveryCodes;
+    /// Recovery codes.
+    final List recoveryCodes;
 
-  MfaRecoveryCodes({
-    required this.recoveryCodes,
-  });
+    MfaRecoveryCodes({
+        required this.recoveryCodes,
+    });
 
-  factory MfaRecoveryCodes.fromMap(Map<String, dynamic> map) {
-    return MfaRecoveryCodes(
-      recoveryCodes: map['recoveryCodes'] ?? [],
-    );
-  }
+    factory MfaRecoveryCodes.fromMap(Map<String, dynamic> map) {
+        return MfaRecoveryCodes(
+            recoveryCodes: map['recoveryCodes'] ?? [],
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {
-      "recoveryCodes": recoveryCodes,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "recoveryCodes": recoveryCodes,
+        };
+    }
 }
