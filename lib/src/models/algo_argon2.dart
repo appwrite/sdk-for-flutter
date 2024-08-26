e47944a07cfef623f427da5ba49a82e0a6f7504e
root <christyjacob4@gmail.com>
2024-08-26 16:48:20 +0000

    chore: update team email address

--- lib/src/models/algo_argon2.dart
+++ lib/src/models/algo_argon2.dart
@@ -2,40 +2,37 @@ part of '../../models.dart';
 
 /// AlgoArgon2
 class AlgoArgon2 implements Model {
-  /// Algo type.
-  final String type;
+    /// Algo type.
+    final String type;
+    /// Memory used to compute hash.
+    final int memoryCost;
+    /// Amount of time consumed to compute hash
+    final int timeCost;
+    /// Number of threads used to compute hash.
+    final int threads;
 
-  /// Memory used to compute hash.
-  final int memoryCost;
+    AlgoArgon2({
+        required this.type,
+        required this.memoryCost,
+        required this.timeCost,
+        required this.threads,
+    });
 
-  /// Amount of time consumed to compute hash
-  final int timeCost;
+    factory AlgoArgon2.fromMap(Map<String, dynamic> map) {
+        return AlgoArgon2(
+            type: map['type'].toString(),
+            memoryCost: map['memoryCost'],
+            timeCost: map['timeCost'],
+            threads: map['threads'],
+        );
+    }
 
-  /// Number of threads used to compute hash.
-  final int threads;
-
-  AlgoArgon2({
-    required this.type,
-    required this.memoryCost,
-    required this.timeCost,
-    required this.threads,
-  });
-
-  factory AlgoArgon2.fromMap(Map<String, dynamic> map) {
-    return AlgoArgon2(
-      type: map['type'].toString(),
-      memoryCost: map['memoryCost'],
-      timeCost: map['timeCost'],
-      threads: map['threads'],
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      "type": type,
-      "memoryCost": memoryCost,
-      "timeCost": timeCost,
-      "threads": threads,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "type": type,
+            "memoryCost": memoryCost,
+            "timeCost": timeCost,
+            "threads": threads,
+        };
+    }
 }
