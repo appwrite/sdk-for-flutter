645b3e0353863ea14c7fe74050ce0cc7fd8fbdf9
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-08-27 15:24:52 +0200

    add changelog

--- lib/id.dart
+++ lib/id.dart
@@ -10,7 +10,8 @@ class ID {
     final now = DateTime.now();
     final sec = (now.millisecondsSinceEpoch / 1000).floor();
     final usec = now.microsecondsSinceEpoch - (sec * 1000000);
-    return sec.toRadixString(16) + usec.toRadixString(16).padLeft(5, '0');
+    return sec.toRadixString(16) +
+        usec.toRadixString(16).padLeft(5, '0');
   }
 
   // Generate a unique ID with padding to have a longer ID
