254426c56df6752b403c4cae9ecbdb3a8b663d1a
root <christyjacob4@gmail.com>
2024-08-27 11:32:27 +0000

    chore: update sdks for appwrite 1.6

--- lib/src/enums/image_format.dart
+++ lib/src/enums/image_format.dart
@@ -1,15 +1,17 @@
 part of '../../enums.dart';
 
 enum ImageFormat {
-  jpg(value: 'jpg'),
-  jpeg(value: 'jpeg'),
-  gif(value: 'gif'),
-  png(value: 'png'),
-  webp(value: 'webp');
+    jpg(value: 'jpg'),
+    jpeg(value: 'jpeg'),
+    gif(value: 'gif'),
+    png(value: 'png'),
+    webp(value: 'webp');
 
-  const ImageFormat({required this.value});
+    const ImageFormat({
+        required this.value
+    });
 
-  final String value;
+    final String value;
 
-  String toJson() => value;
+    String toJson() => value;
 }
