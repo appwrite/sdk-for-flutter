b2d19a8a0702fe9024bb0aea03f3291e0d73ed94
christyjacob4 <christyjacob4@users.noreply.github.com>
2024-08-27 08:52:57 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/execution_method.dart
+++ lib/src/enums/execution_method.dart
@@ -1,18 +1,16 @@
 part of '../../enums.dart';
 
 enum ExecutionMethod {
-    gET(value: 'GET'),
-    pOST(value: 'POST'),
-    pUT(value: 'PUT'),
-    pATCH(value: 'PATCH'),
-    dELETE(value: 'DELETE'),
-    oPTIONS(value: 'OPTIONS');
+  gET(value: 'GET'),
+  pOST(value: 'POST'),
+  pUT(value: 'PUT'),
+  pATCH(value: 'PATCH'),
+  dELETE(value: 'DELETE'),
+  oPTIONS(value: 'OPTIONS');
 
-    const ExecutionMethod({
-        required this.value
-    });
+  const ExecutionMethod({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
--- lib/src/models/algo_scrypt.dart
+++ lib/src/models/algo_scrypt.dart
@@ -2,42 +2,46 @@ part of '../../models.dart';
 
 /// AlgoScrypt
 class AlgoScrypt implements Model {
-    /// Algo type.
-    final String type;
-    /// CPU complexity of computed hash.
-    final int costCpu;
-    /// Memory complexity of computed hash.
-    final int costMemory;
-    /// Parallelization of computed hash.
-    final int costParallel;
-    /// Length used to compute hash.
-    final int length;
+  /// Algo type.
+  final String type;
 
-    AlgoScrypt({
-        required this.type,
-        required this.costCpu,
-        required this.costMemory,
-        required this.costParallel,
-        required this.length,
-    });
+  /// CPU complexity of computed hash.
+  final int costCpu;
 
-    factory AlgoScrypt.fromMap(Map<String, dynamic> map) {
-        return AlgoScrypt(
-            type: map['type'].toString(),
-            costCpu: map['costCpu'],
-            costMemory: map['costMemory'],
-            costParallel: map['costParallel'],
-            length: map['length'],
-        );
-    }
+  /// Memory complexity of computed hash.
+  final int costMemory;
 
-    Map<String, dynamic> toMap() {
-        return {
-            "type": type,
-            "costCpu": costCpu,
-            "costMemory": costMemory,
-            "costParallel": costParallel,
-            "length": length,
-        };
-    }
+  /// Parallelization of computed hash.
+  final int costParallel;
+
+  /// Length used to compute hash.
+  final int length;
+
+  AlgoScrypt({
+    required this.type,
+    required this.costCpu,
+    required this.costMemory,
+    required this.costParallel,
+    required this.length,
+  });
+
+  factory AlgoScrypt.fromMap(Map<String, dynamic> map) {
+    return AlgoScrypt(
+      type: map['type'].toString(),
+      costCpu: map['costCpu'],
+      costMemory: map['costMemory'],
+      costParallel: map['costParallel'],
+      length: map['length'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "type": type,
+      "costCpu": costCpu,
+      "costMemory": costMemory,
+      "costParallel": costParallel,
+      "length": length,
+    };
+  }
 }
