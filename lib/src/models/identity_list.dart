e1cc8455e9f8849bad39f24cbb5744a232420a62
root <christyjacob4@gmail.com>
2024-08-27 08:52:29 +0000

    feat: updates for Appwrite 1.6

--- lib/src/models/identity_list.dart
+++ lib/src/models/identity_list.dart
@@ -2,29 +2,27 @@ part of '../../models.dart';
 
 /// Identities List
 class IdentityList implements Model {
-  /// Total number of identities documents that matched your query.
-  final int total;
+    /// Total number of identities documents that matched your query.
+    final int total;
+    /// List of identities.
+    final List<Identity> identities;
 
-  /// List of identities.
-  final List<Identity> identities;
+    IdentityList({
+        required this.total,
+        required this.identities,
+    });
 
-  IdentityList({
-    required this.total,
-    required this.identities,
-  });
+    factory IdentityList.fromMap(Map<String, dynamic> map) {
+        return IdentityList(
+            total: map['total'],
+            identities: List<Identity>.from(map['identities'].map((p) => Identity.fromMap(p))),
+        );
+    }
 
-  factory IdentityList.fromMap(Map<String, dynamic> map) {
-    return IdentityList(
-      total: map['total'],
-      identities: List<Identity>.from(
-          map['identities'].map((p) => Identity.fromMap(p))),
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      "total": total,
-      "identities": identities.map((p) => p.toMap()).toList(),
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "identities": identities.map((p) => p.toMap()).toList(),
+        };
+    }
 }
--- lib/src/models/template_runtime.dart
+++ lib/src/models/template_runtime.dart
@@ -2,40 +2,37 @@ part of '../../models.dart';
 
 /// Template Runtime
 class TemplateRuntime implements Model {
-  /// Runtime Name.
-  final String name;
+    /// Runtime Name.
+    final String name;
+    /// The build command used to build the deployment.
+    final String commands;
+    /// The entrypoint file used to execute the deployment.
+    final String entrypoint;
+    /// Path to function in VCS (Version Control System) repository
+    final String providerRootDirectory;
 
-  /// The build command used to build the deployment.
-  final String commands;
+    TemplateRuntime({
+        required this.name,
+        required this.commands,
+        required this.entrypoint,
+        required this.providerRootDirectory,
+    });
 
-  /// The entrypoint file used to execute the deployment.
-  final String entrypoint;
+    factory TemplateRuntime.fromMap(Map<String, dynamic> map) {
+        return TemplateRuntime(
+            name: map['name'].toString(),
+            commands: map['commands'].toString(),
+            entrypoint: map['entrypoint'].toString(),
+            providerRootDirectory: map['providerRootDirectory'].toString(),
+        );
+    }
 
-  /// Path to function in VCS (Version Control System) repository
-  final String providerRootDirectory;
-
-  TemplateRuntime({
-    required this.name,
-    required this.commands,
-    required this.entrypoint,
-    required this.providerRootDirectory,
-  });
-
-  factory TemplateRuntime.fromMap(Map<String, dynamic> map) {
-    return TemplateRuntime(
-      name: map['name'].toString(),
-      commands: map['commands'].toString(),
-      entrypoint: map['entrypoint'].toString(),
-      providerRootDirectory: map['providerRootDirectory'].toString(),
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      "name": name,
-      "commands": commands,
-      "entrypoint": entrypoint,
-      "providerRootDirectory": providerRootDirectory,
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "name": name,
+            "commands": commands,
+            "entrypoint": entrypoint,
+            "providerRootDirectory": providerRootDirectory,
+        };
+    }
 }
