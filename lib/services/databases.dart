45e6f754c3433bea472b157e0d5b3c473492e24f
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-03-06 17:13:19 +0000

    Commit from GitHub Actions (Format and push)

--- lib/services/databases.dart
+++ lib/services/databases.dart
@@ -3,104 +3,152 @@ part of '../appwrite.dart';
 /// The Databases service allows you to create structured collections of
 /// documents, query and filter lists of documents
 class Databases extends Service {
-    /// Initializes a [Databases] service
-    Databases(super.client);
+  /// Initializes a [Databases] service
+  Databases(super.client);
 
-    /// Get a list of all the user's documents in a given collection. You can use
-    /// the query params to filter your results.
-    Future<models.DocumentList> listDocuments({required String databaseId, required String collectionId, List<String>? queries}) async {
-        final String apiPath = '/databases/{databaseId}/collections/{collectionId}/documents'.replaceAll('{databaseId}', databaseId).replaceAll('{collectionId}', collectionId);
+  /// Get a list of all the user's documents in a given collection. You can use
+  /// the query params to filter your results.
+  Future<models.DocumentList> listDocuments({
+    required String databaseId,
+    required String collectionId,
+    List<String>? queries,
+  }) async {
+    final String apiPath =
+        '/databases/{databaseId}/collections/{collectionId}/documents'
+            .replaceAll('{databaseId}', databaseId)
+            .replaceAll('{collectionId}', collectionId);
 
-        final Map<String, dynamic> apiParams = {
-            'queries': queries,
-        };
+    final Map<String, dynamic> apiParams = {'queries': queries};
 
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(
+      HttpMethod.get,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        return models.DocumentList.fromMap(res.data);
+    return models.DocumentList.fromMap(res.data);
+  }
 
-    }
+  /// Create a new Document. Before using this route, you should create a new
+  /// collection resource using either a [server
+  /// integration](https://appwrite.io/docs/server/databases#databasesCreateCollection)
+  /// API or directly from your database console.
+  Future<models.Document> createDocument({
+    required String databaseId,
+    required String collectionId,
+    required String documentId,
+    required Map data,
+    List<String>? permissions,
+  }) async {
+    final String apiPath =
+        '/databases/{databaseId}/collections/{collectionId}/documents'
+            .replaceAll('{databaseId}', databaseId)
+            .replaceAll('{collectionId}', collectionId);
 
-    /// Create a new Document. Before using this route, you should create a new
-    /// collection resource using either a [server
-    /// integration](https://appwrite.io/docs/server/databases#databasesCreateCollection)
-    /// API or directly from your database console.
-    Future<models.Document> createDocument({required String databaseId, required String collectionId, required String documentId, required Map data, List<String>? permissions}) async {
-        final String apiPath = '/databases/{databaseId}/collections/{collectionId}/documents'.replaceAll('{databaseId}', databaseId).replaceAll('{collectionId}', collectionId);
+    final Map<String, dynamic> apiParams = {
+      'documentId': documentId,
+      'data': data,
+      'permissions': permissions,
+    };
 
-        final Map<String, dynamic> apiParams = {
-            'documentId': documentId,
-            'data': data,
-            'permissions': permissions,
-        };
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
+    final res = await client.call(
+      HttpMethod.post,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
+    return models.Document.fromMap(res.data);
+  }
 
-        return models.Document.fromMap(res.data);
+  /// Get a document by its unique ID. This endpoint response returns a JSON
+  /// object with the document data.
+  Future<models.Document> getDocument({
+    required String databaseId,
+    required String collectionId,
+    required String documentId,
+    List<String>? queries,
+  }) async {
+    final String apiPath =
+        '/databases/{databaseId}/collections/{collectionId}/documents/{documentId}'
+            .replaceAll('{databaseId}', databaseId)
+            .replaceAll('{collectionId}', collectionId)
+            .replaceAll('{documentId}', documentId);
 
-    }
+    final Map<String, dynamic> apiParams = {'queries': queries};
 
-    /// Get a document by its unique ID. This endpoint response returns a JSON
-    /// object with the document data.
-    Future<models.Document> getDocument({required String databaseId, required String collectionId, required String documentId, List<String>? queries}) async {
-        final String apiPath = '/databases/{databaseId}/collections/{collectionId}/documents/{documentId}'.replaceAll('{databaseId}', databaseId).replaceAll('{collectionId}', collectionId).replaceAll('{documentId}', documentId);
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-        final Map<String, dynamic> apiParams = {
-            'queries': queries,
-        };
+    final res = await client.call(
+      HttpMethod.get,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
+    return models.Document.fromMap(res.data);
+  }
 
-        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
+  /// Update a document by its unique ID. Using the patch method you can pass
+  /// only specific fields that will get updated.
+  Future<models.Document> updateDocument({
+    required String databaseId,
+    required String collectionId,
+    required String documentId,
+    Map? data,
+    List<String>? permissions,
+  }) async {
+    final String apiPath =
+        '/databases/{databaseId}/collections/{collectionId}/documents/{documentId}'
+            .replaceAll('{databaseId}', databaseId)
+            .replaceAll('{collectionId}', collectionId)
+            .replaceAll('{documentId}', documentId);
 
-        return models.Document.fromMap(res.data);
+    final Map<String, dynamic> apiParams = {
+      'data': data,
+      'permissions': permissions,
+    };
 
-    }
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-    /// Update a document by its unique ID. Using the patch method you can pass
-    /// only specific fields that will get updated.
-    Future<models.Document> updateDocument({required String databaseId, required String collectionId, required String documentId, Map? data, List<String>? permissions}) async {
-        final String apiPath = '/databases/{databaseId}/collections/{collectionId}/documents/{documentId}'.replaceAll('{databaseId}', databaseId).replaceAll('{collectionId}', collectionId).replaceAll('{documentId}', documentId);
+    final res = await client.call(
+      HttpMethod.patch,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        final Map<String, dynamic> apiParams = {
-            'data': data,
-            'permissions': permissions,
-        };
+    return models.Document.fromMap(res.data);
+  }
 
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
+  /// Delete a document by its unique ID.
+  Future deleteDocument({
+    required String databaseId,
+    required String collectionId,
+    required String documentId,
+  }) async {
+    final String apiPath =
+        '/databases/{databaseId}/collections/{collectionId}/documents/{documentId}'
+            .replaceAll('{databaseId}', databaseId)
+            .replaceAll('{collectionId}', collectionId)
+            .replaceAll('{documentId}', documentId);
 
-        final res = await client.call(HttpMethod.patch, path: apiPath, params: apiParams, headers: apiHeaders);
+    final Map<String, dynamic> apiParams = {};
 
-        return models.Document.fromMap(res.data);
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-    }
+    final res = await client.call(
+      HttpMethod.delete,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-    /// Delete a document by its unique ID.
-    Future deleteDocument({required String databaseId, required String collectionId, required String documentId}) async {
-        final String apiPath = '/databases/{databaseId}/collections/{collectionId}/documents/{documentId}'.replaceAll('{databaseId}', databaseId).replaceAll('{collectionId}', collectionId).replaceAll('{documentId}', documentId);
-
-        final Map<String, dynamic> apiParams = {
-        };
-
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
-
-        final res = await client.call(HttpMethod.delete, path: apiPath, params: apiParams, headers: apiHeaders);
-
-        return  res.data;
-
-    }
+    return res.data;
+  }
 }
