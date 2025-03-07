e73ab9a038bc93387b4030e89ea0c63cb1e56c22
root <chiragaggarwal5k@gmail.com>
2025-03-07 04:08:07 +0000

    chore: regenerate sdk

--- lib/services/locale.dart
+++ lib/services/locale.dart
@@ -3,163 +3,155 @@ part of '../appwrite.dart';
 /// The Locale service allows you to customize your app based on your users&#039;
 /// location.
 class Locale extends Service {
-  /// Initializes a [Locale] service
-  Locale(super.client);
+    /// Initializes a [Locale] service
+    Locale(super.client);
 
-  /// Get the current user location based on IP. Returns an object with user
-  /// country code, country name, continent name, continent code, ip address and
-  /// suggested currency. You can use the locale header to get the data in a
-  /// supported language.
-  ///
-  /// ([IP Geolocation by DB-IP](https://db-ip.com))
-  Future<models.Locale> get() async {
-    const String apiPath = '/locale';
+    /// Get the current user location based on IP. Returns an object with user
+    /// country code, country name, continent name, continent code, ip address and
+    /// suggested currency. You can use the locale header to get the data in a
+    /// supported language.
+    /// 
+    /// ([IP Geolocation by DB-IP](https://db-ip.com))
+    Future<models.Locale> get() async {
+        const String apiPath = '/locale';
 
-    final Map<String, dynamic> apiParams = {};
+        final Map<String, dynamic> apiParams = {
+        };
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
-    return models.Locale.fromMap(res.data);
-  }
+        return models.Locale.fromMap(res.data);
 
-  /// List of all locale codes in [ISO
-  /// 639-1](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes).
-  Future<models.LocaleCodeList> listCodes() async {
-    const String apiPath = '/locale/codes';
+    }
 
-    final Map<String, dynamic> apiParams = {};
+    /// List of all locale codes in [ISO
+    /// 639-1](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes).
+    Future<models.LocaleCodeList> listCodes() async {
+        const String apiPath = '/locale/codes';
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+        final Map<String, dynamic> apiParams = {
+        };
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    return models.LocaleCodeList.fromMap(res.data);
-  }
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
-  /// List of all continents. You can use the locale header to get the data in a
-  /// supported language.
-  Future<models.ContinentList> listContinents() async {
-    const String apiPath = '/locale/continents';
+        return models.LocaleCodeList.fromMap(res.data);
 
-    final Map<String, dynamic> apiParams = {};
+    }
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+    /// List of all continents. You can use the locale header to get the data in a
+    /// supported language.
+    Future<models.ContinentList> listContinents() async {
+        const String apiPath = '/locale/continents';
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+        final Map<String, dynamic> apiParams = {
+        };
 
-    return models.ContinentList.fromMap(res.data);
-  }
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-  /// List of all countries. You can use the locale header to get the data in a
-  /// supported language.
-  Future<models.CountryList> listCountries() async {
-    const String apiPath = '/locale/countries';
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
-    final Map<String, dynamic> apiParams = {};
+        return models.ContinentList.fromMap(res.data);
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+    }
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+    /// List of all countries. You can use the locale header to get the data in a
+    /// supported language.
+    Future<models.CountryList> listCountries() async {
+        const String apiPath = '/locale/countries';
 
-    return models.CountryList.fromMap(res.data);
-  }
+        final Map<String, dynamic> apiParams = {
+        };
 
-  /// List of all countries that are currently members of the EU. You can use the
-  /// locale header to get the data in a supported language.
-  Future<models.CountryList> listCountriesEU() async {
-    const String apiPath = '/locale/countries/eu';
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    final Map<String, dynamic> apiParams = {};
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+        return models.CountryList.fromMap(res.data);
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+    }
 
-    return models.CountryList.fromMap(res.data);
-  }
+    /// List of all countries that are currently members of the EU. You can use the
+    /// locale header to get the data in a supported language.
+    Future<models.CountryList> listCountriesEU() async {
+        const String apiPath = '/locale/countries/eu';
 
-  /// List of all countries phone codes. You can use the locale header to get the
-  /// data in a supported language.
-  Future<models.PhoneList> listCountriesPhones() async {
-    const String apiPath = '/locale/countries/phones';
+        final Map<String, dynamic> apiParams = {
+        };
 
-    final Map<String, dynamic> apiParams = {};
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+        return models.CountryList.fromMap(res.data);
 
-    return models.PhoneList.fromMap(res.data);
-  }
+    }
 
-  /// List of all currencies, including currency symbol, name, plural, and
-  /// decimal digits for all major and minor currencies. You can use the locale
-  /// header to get the data in a supported language.
-  Future<models.CurrencyList> listCurrencies() async {
-    const String apiPath = '/locale/currencies';
+    /// List of all countries phone codes. You can use the locale header to get the
+    /// data in a supported language.
+    Future<models.PhoneList> listCountriesPhones() async {
+        const String apiPath = '/locale/countries/phones';
 
-    final Map<String, dynamic> apiParams = {};
+        final Map<String, dynamic> apiParams = {
+        };
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
 
-    return models.CurrencyList.fromMap(res.data);
-  }
+        return models.PhoneList.fromMap(res.data);
 
-  /// List of all languages classified by ISO 639-1 including 2-letter code, name
-  /// in English, and name in the respective language.
-  Future<models.LanguageList> listLanguages() async {
-    const String apiPath = '/locale/languages';
+    }
 
-    final Map<String, dynamic> apiParams = {};
+    /// List of all currencies, including currency symbol, name, plural, and
+    /// decimal digits for all major and minor currencies. You can use the locale
+    /// header to get the data in a supported language.
+    Future<models.CurrencyList> listCurrencies() async {
+        const String apiPath = '/locale/currencies';
 
-    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
+        final Map<String, dynamic> apiParams = {
+        };
 
-    final res = await client.call(
-      HttpMethod.get,
-      path: apiPath,
-      params: apiParams,
-      headers: apiHeaders,
-    );
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
 
-    return models.LanguageList.fromMap(res.data);
-  }
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
+
+        return models.CurrencyList.fromMap(res.data);
+
+    }
+
+    /// List of all languages classified by ISO 639-1 including 2-letter code, name
+    /// in English, and name in the respective language.
+    Future<models.LanguageList> listLanguages() async {
+        const String apiPath = '/locale/languages';
+
+        final Map<String, dynamic> apiParams = {
+        };
+
+        final Map<String, String> apiHeaders = {
+            'content-type': 'application/json',
+        };
+
+        final res = await client.call(HttpMethod.get, path: apiPath, params: apiParams, headers: apiHeaders);
+
+        return models.LanguageList.fromMap(res.data);
+
+    }
 }
