6284b60c9e759ce8738c53a02815a9466ace9c29
loks0n <loks0n@users.noreply.github.com>
2025-04-17 18:52:19 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/headers.dart
+++ lib/src/models/headers.dart
@@ -2,28 +2,22 @@ part of '../../models.dart';
 
 /// Headers
 class Headers implements Model {
-    /// Header name.
-    final String name;
+  /// Header name.
+  final String name;
 
-    /// Header value.
-    final String value;
+  /// Header value.
+  final String value;
 
-    Headers({
-        required this.name,
-        required this.value,
-    });
+  Headers({required this.name, required this.value});
 
-    factory Headers.fromMap(Map<String, dynamic> map) {
-        return Headers(
-            name: map['name'].toString(),
-            value: map['value'].toString(),
-        );
-    }
+  factory Headers.fromMap(Map<String, dynamic> map) {
+    return Headers(
+      name: map['name'].toString(),
+      value: map['value'].toString(),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "name": name,
-            "value": value,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"name": name, "value": value};
+  }
 }
