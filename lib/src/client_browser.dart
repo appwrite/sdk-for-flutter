3d5111f00fa80880798e22dd681b4a1eae2bd235
root <chiragaggarwal5k@gmail.com>
2025-04-25 17:53:03 +0000

    chore: regenerate sdk

--- lib/src/client_browser.dart
+++ lib/src/client_browser.dart
@@ -12,11 +12,14 @@ import 'input_file.dart';
 import 'upload_progress.dart';
 import 'response.dart';
 
-ClientBase createClient({required String endPoint, required bool selfSigned}) =>
+ClientBase createClient({
+  required String endPoint,
+  required bool selfSigned,
+}) =>
     ClientBrowser(endPoint: endPoint, selfSigned: selfSigned);
 
 class ClientBrowser extends ClientBase with ClientMixin {
-  static const int CHUNK_SIZE = 5 * 1024 * 1024;
+  static const int CHUNK_SIZE = 5*1024*1024;
   String _endPoint;
   Map<String, String>? _headers;
   @override
@@ -46,10 +49,8 @@ class ClientBrowser extends ClientBase with ClientMixin {
 
     config = {};
 
-    assert(
-      _endPoint.startsWith(RegExp("http://|https://")),
-      "endPoint $_endPoint must start with 'http'",
-    );
+    assert(_endPoint.startsWith(RegExp("http://|https://")),
+        "endPoint $_endPoint must start with 'http'");
     init();
   }
 
@@ -63,7 +64,6 @@ class ClientBrowser extends ClientBase with ClientMixin {
     addHeader('X-Appwrite-Project', value);
     return this;
   }
-
   /// Your secret JSON Web Token
   @override
   ClientBrowser setJWT(value) {
@@ -71,14 +71,12 @@ class ClientBrowser extends ClientBase with ClientMixin {
     addHeader('X-Appwrite-JWT', value);
     return this;
   }
-
   @override
   ClientBrowser setLocale(value) {
     config['locale'] = value;
     addHeader('X-Appwrite-Locale', value);
     return this;
   }
-
   /// The user session to authenticate with
   @override
   ClientBrowser setSession(value) {
@@ -149,11 +147,7 @@ class ClientBrowser extends ClientBase with ClientMixin {
 
     late Response res;
     if (size <= CHUNK_SIZE) {
-      params[paramName] = http.MultipartFile.fromBytes(
-        paramName,
-        file.bytes!,
-        filename: file.filename,
-      );
+      params[paramName] = http.MultipartFile.fromBytes(paramName, file.bytes!, filename: file.filename);
       return call(
         HttpMethod.post,
         path: path,
@@ -180,19 +174,12 @@ class ClientBrowser extends ClientBase with ClientMixin {
       List<int> chunk = [];
       final end = min(offset + CHUNK_SIZE, size);
       chunk = file.bytes!.getRange(offset, end).toList();
-      params[paramName] = http.MultipartFile.fromBytes(
-        paramName,
-        chunk,
-        filename: file.filename,
-      );
+      params[paramName] =
+          http.MultipartFile.fromBytes(paramName, chunk, filename: file.filename);
       headers['content-range'] =
           'bytes $offset-${min<int>((offset + CHUNK_SIZE - 1), size - 1)}/$size';
-      res = await call(
-        HttpMethod.post,
-        path: path,
-        headers: headers,
-        params: params,
-      );
+      res = await call(HttpMethod.post,
+          path: path, headers: headers, params: params);
       offset += CHUNK_SIZE;
       if (offset < size) {
         headers['x-appwrite-id'] = res.data['\$id'];
@@ -233,8 +220,7 @@ class ClientBrowser extends ClientBase with ClientMixin {
       final cookieFallback = res.headers['x-fallback-cookies'];
       if (cookieFallback != null) {
         debugPrint(
-          'Appwrite is using localStorage for session management. Increase your security by adding a custom domain as your API endpoint.',
-        );
+            'Appwrite is using localStorage for session management. Increase your security by adding a custom domain as your API endpoint.');
         addHeader('X-Fallback-Cookies', cookieFallback);
         web.window.localStorage['cookieFallback'] = cookieFallback;
       }
@@ -249,7 +235,7 @@ class ClientBrowser extends ClientBase with ClientMixin {
 
   @override
   Future webAuth(Uri url, {String? callbackUrlScheme}) {
-    return FlutterWebAuth2.authenticate(
+  return FlutterWebAuth2.authenticate(
       url: url.toString(),
       callbackUrlScheme: "appwrite-callback-" + config['project']!,
     );
