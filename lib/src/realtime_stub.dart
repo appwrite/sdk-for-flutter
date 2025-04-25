d99d99e263661a9084905b4c7619df85d7250f3b
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-04-25 19:23:01 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/realtime_stub.dart
+++ lib/src/realtime_stub.dart
@@ -2,5 +2,7 @@ import 'realtime_base.dart';
 import 'client.dart';
 
 /// Implemented in `realtime_browser.dart` and `realtime_io.dart`.
-RealtimeBase createRealtime(Client client) => throw UnsupportedError(
-    'Cannot create a client without dart:html or dart:io.');
+RealtimeBase createRealtime(Client client) =>
+    throw UnsupportedError(
+      'Cannot create a client without dart:html or dart:io.',
+    );
