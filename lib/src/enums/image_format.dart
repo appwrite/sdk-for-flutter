90ffb7c10585e73a3549dab99851be6a9649ee83
root <chiragaggarwal5k@gmail.com>
2025-05-18 08:01:44 +0000

    chore: add changelog

--- lib/src/enums/image_format.dart
+++ lib/src/enums/image_format.dart
@@ -1,16 +1,18 @@
 part of '../../enums.dart';
 
 enum ImageFormat {
-  jpg(value: 'jpg'),
-  jpeg(value: 'jpeg'),
-  png(value: 'png'),
-  webp(value: 'webp'),
-  heic(value: 'heic'),
-  avif(value: 'avif');
+    jpg(value: 'jpg'),
+    jpeg(value: 'jpeg'),
+    png(value: 'png'),
+    webp(value: 'webp'),
+    heic(value: 'heic'),
+    avif(value: 'avif');
 
-  const ImageFormat({required this.value});
+    const ImageFormat({
+        required this.value
+    });
 
-  final String value;
+    final String value;
 
-  String toJson() => value;
+    String toJson() => value;
 }
