ee53d24c924f615890fea42698b8ff94be0517c2
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-05-18 08:02:07 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/algo_md5.dart
+++ lib/src/models/algo_md5.dart
@@ -2,22 +2,16 @@ part of '../../models.dart';
 
 /// AlgoMD5
 class AlgoMd5 implements Model {
-    /// Algo type.
-    final String type;
+  /// Algo type.
+  final String type;
 
-    AlgoMd5({
-        required this.type,
-    });
+  AlgoMd5({required this.type});
 
-    factory AlgoMd5.fromMap(Map<String, dynamic> map) {
-        return AlgoMd5(
-            type: map['type'].toString(),
-        );
-    }
+  factory AlgoMd5.fromMap(Map<String, dynamic> map) {
+    return AlgoMd5(type: map['type'].toString());
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "type": type,
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"type": type};
+  }
 }
