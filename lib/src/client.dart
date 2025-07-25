484e7d0cdd4d8e6ca0cfce8291ffd5eab9aaadab
root <chiragaggarwal5k@gmail.com>
2025-07-25 05:13:04 +0000

    add changelog

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -10,8 +10,7 @@ import 'upload_progress.dart';
 /// The [Client] is also responsible for managing user's sessions.
 abstract class Client {
   /// The size for cunked uploads in bytes.
-  static const int CHUNK_SIZE = 5 * 1024 * 1024;
-
+  static const int CHUNK_SIZE = 5*1024*1024;
   /// Holds configuration such as project.
   late Map<String, String> config;
   late String _endPoint;
@@ -19,15 +18,14 @@ abstract class Client {
 
   /// Appwrite endpoint.
   String get endPoint => _endPoint;
-
   /// Appwrite realtime endpoint.
   String? get endPointRealtime => _endPointRealtime;
 
   /// Initializes a [Client].
-  factory Client({
-    String endPoint = 'https://cloud.appwrite.io/v1',
-    bool selfSigned = false,
-  }) => createClient(endPoint: endPoint, selfSigned: selfSigned);
+  factory Client(
+          {String endPoint = 'https://cloud.appwrite.io/v1',
+          bool selfSigned = false}) =>
+      createClient(endPoint: endPoint, selfSigned: selfSigned);
 
   /// Handle OAuth2 session creation.
   Future webAuth(Uri url, {String? callbackUrlScheme});
@@ -43,7 +41,7 @@ abstract class Client {
   });
 
   /// Set self signed to [status].
-  ///
+  /// 
   /// If self signed is true, [Client] will ignore invalid certificates.
   /// This is helpful in environments where your Appwrite
   /// instance does not have a valid SSL certificate.
@@ -85,8 +83,7 @@ abstract class Client {
   Future<String> ping();
 
   /// Send the API request.
-  Future<Response> call(
-    HttpMethod method, {
+  Future<Response> call(HttpMethod method, {
     String path = '',
     Map<String, String> headers = const {},
     Map<String, dynamic> params = const {},
