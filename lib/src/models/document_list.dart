bb2cde1810f1329d14697fffd55415471166b997
stnguyen90 <stnguyen90@users.noreply.github.com>
2025-08-19 01:13:52 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/document_list.dart
+++ lib/src/models/document_list.dart
@@ -2,31 +2,30 @@ part of '../../models.dart';
 
 /// Documents List
 class DocumentList implements Model {
-    /// Total number of documents documents that matched your query.
-    final int total;
+  /// Total number of documents documents that matched your query.
+  final int total;
 
-    /// List of documents.
-    final List<Document> documents;
+  /// List of documents.
+  final List<Document> documents;
 
-    DocumentList({
-        required this.total,
-        required this.documents,
-    });
+  DocumentList({required this.total, required this.documents});
 
-    factory DocumentList.fromMap(Map<String, dynamic> map) {
-        return DocumentList(
-            total: map['total'],
-            documents: List<Document>.from(map['documents'].map((p) => Document.fromMap(p))),
-        );
-    }
+  factory DocumentList.fromMap(Map<String, dynamic> map) {
+    return DocumentList(
+      total: map['total'],
+      documents: List<Document>.from(
+        map['documents'].map((p) => Document.fromMap(p)),
+      ),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "documents": documents.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {
+      "total": total,
+      "documents": documents.map((p) => p.toMap()).toList(),
+    };
+  }
 
-    List<T> convertTo<T>(T Function(Map) fromJson) =>
-        documents.map((d) => d.convertTo<T>(fromJson)).toList();
+  List<T> convertTo<T>(T Function(Map) fromJson) =>
+      documents.map((d) => d.convertTo<T>(fromJson)).toList();
 }
