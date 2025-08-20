18c53517e74217ec8eb57b4c86cfd84e3751c4b9
Jake Barnby <jakeb994@gmail.com>
2025-08-20 18:22:46 +1200

    Add 1.8.x support

--- lib/src/models/country_list.dart
+++ lib/src/models/country_list.dart
@@ -2,27 +2,28 @@ part of '../../models.dart';
 
 /// Countries List
 class CountryList implements Model {
-  /// Total number of countries documents that matched your query.
-  final int total;
+    /// Total number of countries rows that matched your query.
+    final int total;
 
-  /// List of countries.
-  final List<Country> countries;
+    /// List of countries.
+    final List<Country> countries;
 
-  CountryList({required this.total, required this.countries});
+    CountryList({
+        required this.total,
+        required this.countries,
+    });
 
-  factory CountryList.fromMap(Map<String, dynamic> map) {
-    return CountryList(
-      total: map['total'],
-      countries: List<Country>.from(
-        map['countries'].map((p) => Country.fromMap(p)),
-      ),
-    );
-  }
+    factory CountryList.fromMap(Map<String, dynamic> map) {
+        return CountryList(
+            total: map['total'],
+            countries: List<Country>.from(map['countries'].map((p) => Country.fromMap(p))),
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {
-      "total": total,
-      "countries": countries.map((p) => p.toMap()).toList(),
-    };
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "countries": countries.map((p) => p.toMap()).toList(),
+        };
+    }
 }
