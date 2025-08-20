beace6a3c72cb26433897192b5bcc6a57547886d
Jake Barnby <jakeb994@gmail.com>
2025-08-20 18:41:29 +1200

    Update doc

--- lib/src/models/log_list.dart
+++ lib/src/models/log_list.dart
@@ -2,22 +2,28 @@ part of '../../models.dart';
 
 /// Logs List
 class LogList implements Model {
-  /// Total number of logs rows that matched your query.
-  final int total;
+    /// Total number of logs rows that matched your query.
+    final int total;
 
-  /// List of logs.
-  final List<Log> logs;
+    /// List of logs.
+    final List<Log> logs;
 
-  LogList({required this.total, required this.logs});
+    LogList({
+        required this.total,
+        required this.logs,
+    });
 
-  factory LogList.fromMap(Map<String, dynamic> map) {
-    return LogList(
-      total: map['total'],
-      logs: List<Log>.from(map['logs'].map((p) => Log.fromMap(p))),
-    );
-  }
+    factory LogList.fromMap(Map<String, dynamic> map) {
+        return LogList(
+            total: map['total'],
+            logs: List<Log>.from(map['logs'].map((p) => Log.fromMap(p))),
+        );
+    }
 
-  Map<String, dynamic> toMap() {
-    return {"total": total, "logs": logs.map((p) => p.toMap()).toList()};
-  }
+    Map<String, dynamic> toMap() {
+        return {
+            "total": total,
+            "logs": logs.map((p) => p.toMap()).toList(),
+        };
+    }
 }
