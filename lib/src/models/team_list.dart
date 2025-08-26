ac38bb8b2f04f8c9c9dd11c80394adae9609f6f1
abnegate <abnegate@users.noreply.github.com>
2025-08-26 07:09:10 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/team_list.dart
+++ lib/src/models/team_list.dart
@@ -2,28 +2,22 @@ part of '../../models.dart';
 
 /// Teams List
 class TeamList implements Model {
-    /// Total number of teams that matched your query.
-    final int total;
+  /// Total number of teams that matched your query.
+  final int total;
 
-    /// List of teams.
-    final List<Team> teams;
+  /// List of teams.
+  final List<Team> teams;
 
-    TeamList({
-        required this.total,
-        required this.teams,
-    });
+  TeamList({required this.total, required this.teams});
 
-    factory TeamList.fromMap(Map<String, dynamic> map) {
-        return TeamList(
-            total: map['total'],
-            teams: List<Team>.from(map['teams'].map((p) => Team.fromMap(p))),
-        );
-    }
+  factory TeamList.fromMap(Map<String, dynamic> map) {
+    return TeamList(
+      total: map['total'],
+      teams: List<Team>.from(map['teams'].map((p) => Team.fromMap(p))),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "teams": teams.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"total": total, "teams": teams.map((p) => p.toMap()).toList()};
+  }
 }
