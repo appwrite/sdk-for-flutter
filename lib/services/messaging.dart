32dc4cbf4b51779a04618f83fdcaa9aa820da125
abnegate <abnegate@users.noreply.github.com>
2025-09-04 13:32:40 +0000

    Commit from GitHub Actions (Format and push)

--- lib/services/messaging.dart
+++ lib/services/messaging.dart
@@ -1,44 +1,60 @@
 part of '../appwrite.dart';
 
-  /// The Messaging service allows you to send messages to any provider type
-  /// (SMTP, push notification, SMS, etc.).
+/// The Messaging service allows you to send messages to any provider type
+/// (SMTP, push notification, SMS, etc.).
 class Messaging extends Service {
   /// Initializes a [Messaging] service
   Messaging(super.client);
 
   /// Create a new subscriber.
-  Future<models.Subscriber> createSubscriber({required String topicId, required String subscriberId, required String targetId}) async {
-    final String apiPath = '/messaging/topics/{topicId}/subscribers'.replaceAll('{topicId}', topicId);
+  Future<models.Subscriber> createSubscriber({
+    required String topicId,
+    required String subscriberId,
+    required String targetId,
+  }) async {
+    final String apiPath = '/messaging/topics/{topicId}/subscribers'.replaceAll(
+      '{topicId}',
+      topicId,
+    );
 
-        final Map<String, dynamic> apiParams = {
-            'subscriberId': subscriberId,
-            'targetId': targetId,
-        };
+    final Map<String, dynamic> apiParams = {
+      'subscriberId': subscriberId,
+      'targetId': targetId,
+    };
 
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(
+      HttpMethod.post,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        return models.Subscriber.fromMap(res.data);
-
+    return models.Subscriber.fromMap(res.data);
   }
 
   /// Delete a subscriber by its unique ID.
-  Future deleteSubscriber({required String topicId, required String subscriberId}) async {
-    final String apiPath = '/messaging/topics/{topicId}/subscribers/{subscriberId}'.replaceAll('{topicId}', topicId).replaceAll('{subscriberId}', subscriberId);
+  Future deleteSubscriber({
+    required String topicId,
+    required String subscriberId,
+  }) async {
+    final String apiPath =
+        '/messaging/topics/{topicId}/subscribers/{subscriberId}'
+            .replaceAll('{topicId}', topicId)
+            .replaceAll('{subscriberId}', subscriberId);
 
-        final Map<String, dynamic> apiParams = {
-        };
+    final Map<String, dynamic> apiParams = {};
 
-        final Map<String, String> apiHeaders = {
-            'content-type': 'application/json',
-        };
+    final Map<String, String> apiHeaders = {'content-type': 'application/json'};
 
-        final res = await client.call(HttpMethod.delete, path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(
+      HttpMethod.delete,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        return  res.data;
-
+    return res.data;
   }
 }
