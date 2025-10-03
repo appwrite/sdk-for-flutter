acd8e688fbadf0177f08170b8869ef85a66bcb4c
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-10-03 03:06:34 +0000

    Commit from GitHub Actions (Format and push)

--- lib/services/graphql.dart
+++ lib/services/graphql.dart
@@ -1,7 +1,7 @@
 part of '../appwrite.dart';
 
-  /// The GraphQL API allows you to query and mutate your Appwrite server using
-  /// GraphQL.
+/// The GraphQL API allows you to query and mutate your Appwrite server using
+/// GraphQL.
 class Graphql extends Service {
   /// Initializes a [Graphql] service
   Graphql(super.client);
@@ -10,35 +10,41 @@ class Graphql extends Service {
   Future query({required Map query}) async {
     const String apiPath = '/graphql';
 
-        final Map<String, dynamic> apiParams = {
-            'query': query,
-        };
+    final Map<String, dynamic> apiParams = {'query': query};
 
-        final Map<String, String> apiHeaders = {
-            'x-sdk-graphql': 'true',            'content-type': 'application/json',
-        };
+    final Map<String, String> apiHeaders = {
+      'x-sdk-graphql': 'true',
+      'content-type': 'application/json',
+    };
 
-        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(
+      HttpMethod.post,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        return  res.data;
-
+    return res.data;
   }
 
   /// Execute a GraphQL mutation.
   Future mutation({required Map query}) async {
     const String apiPath = '/graphql/mutation';
 
-        final Map<String, dynamic> apiParams = {
-            'query': query,
-        };
+    final Map<String, dynamic> apiParams = {'query': query};
 
-        final Map<String, String> apiHeaders = {
-            'x-sdk-graphql': 'true',            'content-type': 'application/json',
-        };
+    final Map<String, String> apiHeaders = {
+      'x-sdk-graphql': 'true',
+      'content-type': 'application/json',
+    };
 
-        final res = await client.call(HttpMethod.post, path: apiPath, params: apiParams, headers: apiHeaders);
+    final res = await client.call(
+      HttpMethod.post,
+      path: apiPath,
+      params: apiParams,
+      headers: apiHeaders,
+    );
 
-        return  res.data;
-
+    return res.data;
   }
 }
--- lib/src/models/row_list.dart
+++ lib/src/models/row_list.dart
@@ -2,31 +2,25 @@ part of '../../models.dart';
 
 /// Rows List
 class RowList implements Model {
-    /// Total number of rows that matched your query.
-    final int total;
+  /// Total number of rows that matched your query.
+  final int total;
 
-    /// List of rows.
-    final List<Row> rows;
+  /// List of rows.
+  final List<Row> rows;
 
-    RowList({
-        required this.total,
-        required this.rows,
-    });
+  RowList({required this.total, required this.rows});
 
-    factory RowList.fromMap(Map<String, dynamic> map) {
-        return RowList(
-            total: map['total'],
-            rows: List<Row>.from(map['rows'].map((p) => Row.fromMap(p))),
-        );
-    }
+  factory RowList.fromMap(Map<String, dynamic> map) {
+    return RowList(
+      total: map['total'],
+      rows: List<Row>.from(map['rows'].map((p) => Row.fromMap(p))),
+    );
+  }
 
-    Map<String, dynamic> toMap() {
-        return {
-            "total": total,
-            "rows": rows.map((p) => p.toMap()).toList(),
-        };
-    }
+  Map<String, dynamic> toMap() {
+    return {"total": total, "rows": rows.map((p) => p.toMap()).toList()};
+  }
 
-    List<T> convertTo<T>(T Function(Map) fromJson) =>
-        rows.map((d) => d.convertTo<T>(fromJson)).toList();
+  List<T> convertTo<T>(T Function(Map) fromJson) =>
+      rows.map((d) => d.convertTo<T>(fromJson)).toList();
 }
