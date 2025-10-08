c11a4e59266da26b98c4fe3023f199ba7aca18b0
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2025-10-08 04:14:11 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/client_browser.dart
+++ lib/src/client_browser.dart
@@ -63,6 +63,7 @@ class ClientBrowser extends ClientBase with ClientMixin {
     addHeader('X-Appwrite-Project', value);
     return this;
   }
+
   /// Your secret JSON Web Token
   @override
   ClientBrowser setJWT(value) {
@@ -70,12 +71,14 @@ class ClientBrowser extends ClientBase with ClientMixin {
     addHeader('X-Appwrite-JWT', value);
     return this;
   }
+
   @override
   ClientBrowser setLocale(value) {
     config['locale'] = value;
     addHeader('X-Appwrite-Locale', value);
     return this;
   }
+
   /// The user session to authenticate with
   @override
   ClientBrowser setSession(value) {
@@ -83,6 +86,7 @@ class ClientBrowser extends ClientBase with ClientMixin {
     addHeader('X-Appwrite-Session', value);
     return this;
   }
+
   /// Your secret dev API key
   @override
   ClientBrowser setDevKey(value) {
