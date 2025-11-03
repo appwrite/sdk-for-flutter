c7575b8ce106607559b46d61767d8eff7db97f6c
abnegate <abnegate@users.noreply.github.com>
2025-11-03 04:38:08 +0000

    Commit from GitHub Actions (Format and push)

--- test/src/models/language_list_test.dart
+++ test/src/models/language_list_test.dart
@@ -12,8 +12,8 @@ void main() {
       final map = model.toMap();
       final result = LanguageList.fromMap(map);
 
-            expect(result.total, 5);
-                  expect(result.languages, []);
-          });
+      expect(result.total, 5);
+      expect(result.languages, []);
+    });
   });
 }
