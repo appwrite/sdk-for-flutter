2f803e7e37fe03012758b7eec19dbc13a321e3ce
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-02-12 05:45:29 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/enums/image_format.dart
+++ lib/src/enums/image_format.dart
@@ -1,19 +1,17 @@
 part of '../../enums.dart';
 
 enum ImageFormat {
-    jpg(value: 'jpg'),
-    jpeg(value: 'jpeg'),
-    png(value: 'png'),
-    webp(value: 'webp'),
-    heic(value: 'heic'),
-    avif(value: 'avif'),
-    gif(value: 'gif');
+  jpg(value: 'jpg'),
+  jpeg(value: 'jpeg'),
+  png(value: 'png'),
+  webp(value: 'webp'),
+  heic(value: 'heic'),
+  avif(value: 'avif'),
+  gif(value: 'gif');
 
-    const ImageFormat({
-        required this.value
-    });
+  const ImageFormat({required this.value});
 
-    final String value;
+  final String value;
 
-    String toJson() => value;
+  String toJson() => value;
 }
