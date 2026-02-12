a7fa706ba55b9a8804b0aec5de5146f14c71bb28
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-02-12 05:19:58 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/algo_argon2.dart
+++ lib/src/models/algo_argon2.dart
@@ -2,41 +2,41 @@ part of '../../models.dart';
 
 /// AlgoArgon2
 class AlgoArgon2 implements Model {
-    /// Algo type.
-    final String type;
+  /// Algo type.
+  final String type;
 
-    /// Memory used to compute hash.
-    final int memoryCost;
+  /// Memory used to compute hash.
+  final int memoryCost;
 
-    /// Amount of time consumed to compute hash
-    final int timeCost;
+  /// Amount of time consumed to compute hash
+  final int timeCost;
 
-    /// Number of threads used to compute hash.
-    final int threads;
+  /// Number of threads used to compute hash.
+  final int threads;
 
-    AlgoArgon2({
-        required this.type,
-        required this.memoryCost,
-        required this.timeCost,
-        required this.threads,
-    });
+  AlgoArgon2({
+    required this.type,
+    required this.memoryCost,
+    required this.timeCost,
+    required this.threads,
+  });
 
-    factory AlgoArgon2.fromMap(Map<String, dynamic> map) {
-        return AlgoArgon2(
-            type: map['type'].toString(),
-            memoryCost: map['memoryCost'],
-            timeCost: map['timeCost'],
-            threads: map['threads'],
-        );
-    }
+  factory AlgoArgon2.fromMap(Map<String, dynamic> map) {
+    return AlgoArgon2(
+      type: map['type'].toString(),
+      memoryCost: map['memoryCost'],
+      timeCost: map['timeCost'],
+      threads: map['threads'],
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "type": type,
-            "memoryCost": memoryCost,
-            "timeCost": timeCost,
-            "threads": threads,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "type": type,
+      "memoryCost": memoryCost,
+      "timeCost": timeCost,
+      "threads": threads,
+    };
+  }
 }
--- lib/src/models/preferences.dart
+++ lib/src/models/preferences.dart
@@ -2,24 +2,24 @@ part of '../../models.dart';
 
 /// Preferences
 class Preferences implements Model {
-    final Map<String, dynamic> data;
+  final Map<String, dynamic> data;
 
-    Preferences({
-        required this.data,
-    });
+  Preferences({
+    required this.data,
+  });
 
-    factory Preferences.fromMap(Map<String, dynamic> map) {
-        return Preferences(
-            data: map["data"] ?? map,
-        );
-    }
+  factory Preferences.fromMap(Map<String, dynamic> map) {
+    return Preferences(
+      data: map["data"] ?? map,
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "data": data,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "data": data,
+    };
+  }
 
-    T convertTo<T>(T Function(Map<String, dynamic>) fromJson) => fromJson(data);
+  T convertTo<T>(T Function(Map<String, dynamic>) fromJson) => fromJson(data);
 }
