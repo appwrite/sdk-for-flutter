5a93a6337dbf1a699e8a596d0dd49772ea52fdbd
ArnabChatterjee20k <ArnabChatterjee20k@users.noreply.github.com>
2026-02-13 10:02:24 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/key.dart
+++ lib/src/models/key.dart
@@ -2,71 +2,71 @@ part of '../../models.dart';
 
 /// Key
 class Key implements Model {
-    /// Key ID.
-    final String $id;
+  /// Key ID.
+  final String $id;
 
-    /// Key creation date in ISO 8601 format.
-    final String $createdAt;
+  /// Key creation date in ISO 8601 format.
+  final String $createdAt;
 
-    /// Key update date in ISO 8601 format.
-    final String $updatedAt;
+  /// Key update date in ISO 8601 format.
+  final String $updatedAt;
 
-    /// Key name.
-    final String name;
+  /// Key name.
+  final String name;
 
-    /// Key expiration date in ISO 8601 format.
-    final String expire;
+  /// Key expiration date in ISO 8601 format.
+  final String expire;
 
-    /// Allowed permission scopes.
-    final List<String> scopes;
+  /// Allowed permission scopes.
+  final List<String> scopes;
 
-    /// Secret key.
-    final String secret;
+  /// Secret key.
+  final String secret;
 
-    /// Most recent access date in ISO 8601 format. This attribute is only updated again after 24 hours.
-    final String accessedAt;
+  /// Most recent access date in ISO 8601 format. This attribute is only updated again after 24 hours.
+  final String accessedAt;
 
-    /// List of SDK user agents that used this key.
-    final List<String> sdks;
+  /// List of SDK user agents that used this key.
+  final List<String> sdks;
 
-    Key({
-        required this.$id,
-        required this.$createdAt,
-        required this.$updatedAt,
-        required this.name,
-        required this.expire,
-        required this.scopes,
-        required this.secret,
-        required this.accessedAt,
-        required this.sdks,
-    });
+  Key({
+    required this.$id,
+    required this.$createdAt,
+    required this.$updatedAt,
+    required this.name,
+    required this.expire,
+    required this.scopes,
+    required this.secret,
+    required this.accessedAt,
+    required this.sdks,
+  });
 
-    factory Key.fromMap(Map<String, dynamic> map) {
-        return Key(
-            $id: map['\$id'].toString(),
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            name: map['name'].toString(),
-            expire: map['expire'].toString(),
-            scopes: List.from(map['scopes'] ?? []),
-            secret: map['secret'].toString(),
-            accessedAt: map['accessedAt'].toString(),
-            sdks: List.from(map['sdks'] ?? []),
-        );
-    }
+  factory Key.fromMap(Map<String, dynamic> map) {
+    return Key(
+      $id: map['\$id'].toString(),
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      name: map['name'].toString(),
+      expire: map['expire'].toString(),
+      scopes: List.from(map['scopes'] ?? []),
+      secret: map['secret'].toString(),
+      accessedAt: map['accessedAt'].toString(),
+      sdks: List.from(map['sdks'] ?? []),
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "\$id": $id,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "name": name,
-            "expire": expire,
-            "scopes": scopes,
-            "secret": secret,
-            "accessedAt": accessedAt,
-            "sdks": sdks,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "\$id": $id,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "name": name,
+      "expire": expire,
+      "scopes": scopes,
+      "secret": secret,
+      "accessedAt": accessedAt,
+      "sdks": sdks,
+    };
+  }
 }
