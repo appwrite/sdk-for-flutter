53243c16b557b4747780128dd6a1d9e5d15b6b7b
root <arnabchatterjee.ac.2@gmail.com>
2026-03-03 09:07:18 +0000

    updated

--- test/src/models/document_test.dart
+++ test/src/models/document_test.dart
@@ -18,13 +18,13 @@ void main() {
       final map = model.toMap();
       final result = Document.fromMap(map);
 
-      expect(result.$id, '5e5ea5c16897e');
-      expect(result.$sequence, 1);
-      expect(result.$collectionId, '5e5ea5c15117e');
-      expect(result.$databaseId, '5e5ea5c15117e');
-      expect(result.$createdAt, '2020-10-15T06:38:00.000+00:00');
-      expect(result.$updatedAt, '2020-10-15T06:38:00.000+00:00');
-      expect(result.$permissions, []);
-    });
+            expect(result.$id, '5e5ea5c16897e');
+                  expect(result.$sequence, 1);
+                  expect(result.$collectionId, '5e5ea5c15117e');
+                  expect(result.$databaseId, '5e5ea5c15117e');
+                  expect(result.$createdAt, '2020-10-15T06:38:00.000+00:00');
+                  expect(result.$updatedAt, '2020-10-15T06:38:00.000+00:00');
+                  expect(result.$permissions, []);
+          });
   });
 }
