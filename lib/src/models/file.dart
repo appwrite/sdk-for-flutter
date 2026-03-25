061342cf841b10278655d6a7f38033ca35cdd908
root <chiragaggarwal5k@gmail.com>
2026-03-25 04:33:35 +0000

    chore: update Flutter SDK to 23.0.0

--- lib/src/models/file.dart
+++ lib/src/models/file.dart
@@ -2,95 +2,95 @@ part of '../../models.dart';
 
 /// File
 class File implements Model {
-  /// File ID.
-  final String $id;
+    /// File ID.
+    final String $id;
 
-  /// Bucket ID.
-  final String bucketId;
+    /// Bucket ID.
+    final String bucketId;
 
-  /// File creation date in ISO 8601 format.
-  final String $createdAt;
+    /// File creation date in ISO 8601 format.
+    final String $createdAt;
 
-  /// File update date in ISO 8601 format.
-  final String $updatedAt;
+    /// File update date in ISO 8601 format.
+    final String $updatedAt;
 
-  /// File permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
-  final List<String> $permissions;
+    /// File permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
+    final List<String> $permissions;
 
-  /// File name.
-  final String name;
+    /// File name.
+    final String name;
 
-  /// File MD5 signature.
-  final String signature;
+    /// File MD5 signature.
+    final String signature;
 
-  /// File mime type.
-  final String mimeType;
+    /// File mime type.
+    final String mimeType;
 
-  /// File original size in bytes.
-  final int sizeOriginal;
+    /// File original size in bytes.
+    final int sizeOriginal;
 
-  /// Total number of chunks available
-  final int chunksTotal;
+    /// Total number of chunks available
+    final int chunksTotal;
 
-  /// Total number of chunks uploaded
-  final int chunksUploaded;
+    /// Total number of chunks uploaded
+    final int chunksUploaded;
 
-  /// Whether file contents are encrypted at rest.
-  final bool encryption;
+    /// Whether file contents are encrypted at rest.
+    final bool encryption;
 
-  /// Compression algorithm used for the file. Will be one of none, [gzip](https://en.wikipedia.org/wiki/Gzip), or [zstd](https://en.wikipedia.org/wiki/Zstd).
-  final String compression;
+    /// Compression algorithm used for the file. Will be one of none, [gzip](https://en.wikipedia.org/wiki/Gzip), or [zstd](https://en.wikipedia.org/wiki/Zstd).
+    final String compression;
 
-  File({
-    required this.$id,
-    required this.bucketId,
-    required this.$createdAt,
-    required this.$updatedAt,
-    required this.$permissions,
-    required this.name,
-    required this.signature,
-    required this.mimeType,
-    required this.sizeOriginal,
-    required this.chunksTotal,
-    required this.chunksUploaded,
-    required this.encryption,
-    required this.compression,
-  });
+    File({
+        required this.$id,
+        required this.bucketId,
+        required this.$createdAt,
+        required this.$updatedAt,
+        required this.$permissions,
+        required this.name,
+        required this.signature,
+        required this.mimeType,
+        required this.sizeOriginal,
+        required this.chunksTotal,
+        required this.chunksUploaded,
+        required this.encryption,
+        required this.compression,
+    });
 
-  factory File.fromMap(Map<String, dynamic> map) {
-    return File(
-      $id: map['\$id'].toString(),
-      bucketId: map['bucketId'].toString(),
-      $createdAt: map['\$createdAt'].toString(),
-      $updatedAt: map['\$updatedAt'].toString(),
-      $permissions: List.from(map['\$permissions'] ?? []),
-      name: map['name'].toString(),
-      signature: map['signature'].toString(),
-      mimeType: map['mimeType'].toString(),
-      sizeOriginal: map['sizeOriginal'],
-      chunksTotal: map['chunksTotal'],
-      chunksUploaded: map['chunksUploaded'],
-      encryption: map['encryption'],
-      compression: map['compression'].toString(),
-    );
-  }
+    factory File.fromMap(Map<String, dynamic> map) {
+        return File(
+            $id: map['\$id'].toString(),
+            bucketId: map['bucketId'].toString(),
+            $createdAt: map['\$createdAt'].toString(),
+            $updatedAt: map['\$updatedAt'].toString(),
+            $permissions: List.from(map['\$permissions'] ?? []),
+            name: map['name'].toString(),
+            signature: map['signature'].toString(),
+            mimeType: map['mimeType'].toString(),
+            sizeOriginal: map['sizeOriginal'],
+            chunksTotal: map['chunksTotal'],
+            chunksUploaded: map['chunksUploaded'],
+            encryption: map['encryption'],
+            compression: map['compression'].toString(),
+        );
+    }
 
-  @override
-  Map<String, dynamic> toMap() {
-    return {
-      "\$id": $id,
-      "bucketId": bucketId,
-      "\$createdAt": $createdAt,
-      "\$updatedAt": $updatedAt,
-      "\$permissions": $permissions,
-      "name": name,
-      "signature": signature,
-      "mimeType": mimeType,
-      "sizeOriginal": sizeOriginal,
-      "chunksTotal": chunksTotal,
-      "chunksUploaded": chunksUploaded,
-      "encryption": encryption,
-      "compression": compression,
-    };
-  }
+    @override
+    Map<String, dynamic> toMap() {
+        return {
+            "\$id": $id,
+            "bucketId": bucketId,
+            "\$createdAt": $createdAt,
+            "\$updatedAt": $updatedAt,
+            "\$permissions": $permissions,
+            "name": name,
+            "signature": signature,
+            "mimeType": mimeType,
+            "sizeOriginal": sizeOriginal,
+            "chunksTotal": chunksTotal,
+            "chunksUploaded": chunksUploaded,
+            "encryption": encryption,
+            "compression": compression,
+        };
+    }
 }
