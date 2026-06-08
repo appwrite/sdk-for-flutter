fe0ae68ed8677898c53833fef2e21376205909df
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-06-08 05:57:22 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/models/algo_md5.dart
+++ lib/src/models/algo_md5.dart
@@ -2,23 +2,23 @@ part of '../../models.dart';
 
 /// AlgoMD5
 class AlgoMd5 implements Model {
-    /// Algo type.
-    final String type;
+  /// Algo type.
+  final String type;
 
-    AlgoMd5({
-        required this.type,
-    });
+  AlgoMd5({
+    required this.type,
+  });
 
-    factory AlgoMd5.fromMap(Map<String, dynamic> map) {
-        return AlgoMd5(
-            type: map['type'].toString(),
-        );
-    }
+  factory AlgoMd5.fromMap(Map<String, dynamic> map) {
+    return AlgoMd5(
+      type: map['type'].toString(),
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "type": type,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "type": type,
+    };
+  }
 }
--- lib/src/models/presence.dart
+++ lib/src/models/presence.dart
@@ -2,71 +2,71 @@ part of '../../models.dart';
 
 /// Presence
 class Presence implements Model {
-    /// Presence ID.
-    final String $id;
+  /// Presence ID.
+  final String $id;
 
-    /// Presence creation date in ISO 8601 format.
-    final String $createdAt;
+  /// Presence creation date in ISO 8601 format.
+  final String $createdAt;
 
-    /// Presence update date in ISO 8601 format.
-    final String $updatedAt;
+  /// Presence update date in ISO 8601 format.
+  final String $updatedAt;
 
-    /// Presence permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
-    final List<String> $permissions;
+  /// Presence permissions. [Learn more about permissions](https://appwrite.io/docs/permissions).
+  final List<String> $permissions;
 
-    /// User ID.
-    final String userId;
+  /// User ID.
+  final String userId;
 
-    /// Presence status.
-    final String? status;
+  /// Presence status.
+  final String? status;
 
-    /// Presence source.
-    final String source;
+  /// Presence source.
+  final String source;
 
-    /// Presence expiry date in ISO 8601 format.
-    final String? expiresAt;
+  /// Presence expiry date in ISO 8601 format.
+  final String? expiresAt;
 
-    /// Presence metadata.
-    final Map<String, dynamic>? metadata;
+  /// Presence metadata.
+  final Map<String, dynamic>? metadata;
 
-    Presence({
-        required this.$id,
-        required this.$createdAt,
-        required this.$updatedAt,
-        required this.$permissions,
-        required this.userId,
-        this.status,
-        required this.source,
-        this.expiresAt,
-        this.metadata,
-    });
+  Presence({
+    required this.$id,
+    required this.$createdAt,
+    required this.$updatedAt,
+    required this.$permissions,
+    required this.userId,
+    this.status,
+    required this.source,
+    this.expiresAt,
+    this.metadata,
+  });
 
-    factory Presence.fromMap(Map<String, dynamic> map) {
-        return Presence(
-            $id: map['\$id'].toString(),
-            $createdAt: map['\$createdAt'].toString(),
-            $updatedAt: map['\$updatedAt'].toString(),
-            $permissions: List.from(map['\$permissions'] ?? []),
-            userId: map['userId'].toString(),
-            status: map['status']?.toString(),
-            source: map['source'].toString(),
-            expiresAt: map['expiresAt']?.toString(),
-            metadata: map['metadata'],
-        );
-    }
+  factory Presence.fromMap(Map<String, dynamic> map) {
+    return Presence(
+      $id: map['\$id'].toString(),
+      $createdAt: map['\$createdAt'].toString(),
+      $updatedAt: map['\$updatedAt'].toString(),
+      $permissions: List.from(map['\$permissions'] ?? []),
+      userId: map['userId'].toString(),
+      status: map['status']?.toString(),
+      source: map['source'].toString(),
+      expiresAt: map['expiresAt']?.toString(),
+      metadata: map['metadata'],
+    );
+  }
 
-    @override
-    Map<String, dynamic> toMap() {
-        return {
-            "\$id": $id,
-            "\$createdAt": $createdAt,
-            "\$updatedAt": $updatedAt,
-            "\$permissions": $permissions,
-            "userId": userId,
-            "status": status,
-            "source": source,
-            "expiresAt": expiresAt,
-            "metadata": metadata,
-        };
-    }
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      "\$id": $id,
+      "\$createdAt": $createdAt,
+      "\$updatedAt": $updatedAt,
+      "\$permissions": $permissions,
+      "userId": userId,
+      "status": status,
+      "source": source,
+      "expiresAt": expiresAt,
+      "metadata": metadata,
+    };
+  }
 }
