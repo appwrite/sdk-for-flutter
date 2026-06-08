263ab73d432a267e30f4654c3f8e34eb13bf1455
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-06-08 07:24:38 +0000

    Commit from GitHub Actions (Format and push)

--- test/src/models/phone_list_test.dart
+++ test/src/models/phone_list_test.dart
@@ -12,8 +12,8 @@ void main() {
       final map = model.toMap();
       final result = PhoneList.fromMap(map);
 
-            expect(result.total, 5);
-                  expect(result.phones, []);
-          });
+      expect(result.total, 5);
+      expect(result.phones, []);
+    });
   });
 }
