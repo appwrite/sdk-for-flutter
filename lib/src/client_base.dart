e50fec6c973ac0c6430c13277a75e1b3c72d3b1a
ChiragAgg5k <ChiragAgg5k@users.noreply.github.com>
2026-07-13 05:17:04 +0000

    Commit from GitHub Actions (Format and push)

--- lib/src/client_base.dart
+++ lib/src/client_base.dart
@@ -6,29 +6,37 @@ abstract class ClientBase implements Client {
   /// Your project ID
   @override
   ClientBase setProject(value);
+
   /// Your secret JSON Web Token
   @override
   ClientBase setJWT(value);
+
   /// The OAuth access token to authenticate with
   @override
   ClientBase setBearer(value);
   @override
   ClientBase setLocale(value);
+
   /// The user session to authenticate with
   @override
   ClientBase setSession(value);
+
   /// Your secret dev API key
   @override
   ClientBase setDevKey(value);
+
   /// The user cookie to authenticate with. Used by SDKs that forward an incoming Cookie header in server-side runtimes.
   @override
   ClientBase setCookie(value);
+
   /// Impersonate a user by ID
   @override
   ClientBase setImpersonateUserId(value);
+
   /// Impersonate a user by email
   @override
   ClientBase setImpersonateUserEmail(value);
+
   /// Impersonate a user by phone
   @override
   ClientBase setImpersonateUserPhone(value);
